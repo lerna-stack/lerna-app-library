a116e50b4380ea9908128b6dd4e0befec587a5bf
Taichi Yamakawa <yamakawa.taichi@tis.co.jp>
2021-06-03 15:50:51 +0900

    Provide all constructors of ScalaTestWithTypedActorTestKit

--- lerna-testkit/src/main/scala/lerna/testkit/akka/ScalaTestWithTypedActorTestKit.scala
+++ lerna-testkit/src/main/scala/lerna/testkit/akka/ScalaTestWithTypedActorTestKit.scala
@@ -1,7 +1,9 @@
 package lerna.testkit.akka
 
-import akka.actor.testkit.typed.scaladsl.{ ActorTestKit, ScalaTestWithActorTestKit }
+import akka.actor.testkit.typed.TestKitSettings
+import akka.actor.testkit.typed.scaladsl.{ ActorTestKit, ActorTestKitBase, ScalaTestWithActorTestKit }
 import akka.actor.typed.ActorSystem
+import com.typesafe.config.{ Config, ConfigFactory }
 
 /** A test class which improve consistency and reduce boilerplate.
   *
@@ -14,7 +16,19 @@ abstract class ScalaTestWithTypedActorTestKit(testKit: ActorTestKit)
     extends ScalaTestWithActorTestKit(testKit)
     with AkkaTypedSpanScaleFactorSupport {
 
-  def this() = this(ActorTestKit())
-  def this(system: ActorSystem[_]) = this(ActorTestKit(system))
+  def this() =
+    this(ActorTestKit())
+
+  def this(system: ActorSystem[_]) =
+    this(ActorTestKit(system))
+
+  def this(config: String) =
+    this(ActorTestKit(ActorTestKitBase.testNameFromCallStack(), ConfigFactory.parseString(config)))
+
+  def this(config: Config) =
+    this(ActorTestKit(ActorTestKitBase.testNameFromCallStack(), config))
+
+  def this(config: Config, settings: TestKitSettings) =
+    this(ActorTestKit(ActorTestKitBase.testNameFromCallStack(), config, settings))
 
 }
